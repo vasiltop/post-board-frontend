f795e40935e949586c3e66e81842bd5113155b4d
vasiltop <vasiltopalovic@outlook.com>
2023-07-31 16:42:59 -0400

    Home Page Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Home from './pages/Home/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,16 @@
+import './Home.css';
+
 export default function Home() {
     return(
-        <h1>
-            Sup
-        </h1>
+        <div id="centered-content">
+            
+            <div id="post-content-header">
+            
+            </div>
+
+            <div id="post-content">
+            
+            </div>
+        </div>
     )
 }
