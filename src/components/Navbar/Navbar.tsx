84d02aab35a7efe0718d73bf6a0bea50bc76289e
vasiltop <vasiltopalovic@outlook.com>
2023-07-31 17:08:28 -0400

    Navbar Layout

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import './Navbar.css';
+
+export default function Navbar() {
+    return(
+        <div id="navbar-container">
+        <h2> Post Board </h2>
+        <button> Profile </button>
+        </div>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import './Home.css';
+import Navbar from '../../components/Navbar/Navbar';
 
 export default function Home() {
     return(
         <div id="centered-content">
             
             <div id="post-content-header">
-            
+                <Navbar />
             </div>
 
             <div id="post-content">
