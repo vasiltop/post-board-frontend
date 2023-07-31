c0598d31a8d64090594841dd3627f6889c63f3e0
vasiltop <vasiltopalovic@outlook.com>
2023-07-31 16:09:05 -0400

    React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Home from './pages/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+      </Routes> 
+    </BrowserRouter>
   )
 }
 
