9d18c63a17e5bbdc94682b59ccbe12c6206004ce
vasiltop <vasiltopalovic@outlook.com>
2023-09-07 16:22:43 -0400

    feat: Added login and register forms

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,30 @@
 import './Login.css';
+import { useState } from 'react';
+import { onChange } from '../../utils/event';
 
 export default function Login() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(event : React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log({email, password})
+    }
+
     return (
         <>
-        Login
+
+        <form onSubmit={handleLogin}>
+
+        <input type="text" value={email} required onChange={onChange(setEmail)}/>
+
+        <input type="password" value={password} required onChange={onChange(setPassword)}/>
+
+        <button type="submit">Login</button>
+
+        </form>
+
         </>
     )
     
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react'
+import { onChange } from '../../utils/event';
+
 export default function Register() {
 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleRegister(event : React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log({email, username, password});
+    }
 
     return(
         <>
-        Register
+        <form onSubmit={handleRegister}>
+
+        <input value={email} type="text" required onChange={onChange(setEmail)} />
+
+        <input value={username} type="text" required onChange={onChange(setUsername)}/>
+
+        <input value={password} type="password" required onChange={onChange(setPassword)}/> 
+
+        <button type="submit">Login</button>
+
+        </form>
         </>
     )
 }
